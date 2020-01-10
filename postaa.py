8b2692cc1a67f8454edd58f4760d38ac1f48c800
Vilhomaa <34384871+vilhomaa@users.noreply.github.com>
2020-01-10 17:22:11 +0200

    initial upload

---
+++ postaa.py
@@ -0,0 +1,84 @@
+
+import os
+os.chdir("Z:\python\dadjokeai\dadjokevenv")
+import kuvanteko
+os.chdir("Z:\python\dadjokeai")
+import pandas as pd
+import instabot
+import time
+import numpy as np
+from random import sample 
+
+#importing login details for the ig account
+import sys
+
+sys.path.insert(1, 'Z:\python')
+
+from salasanat import dadjokelogin
+
+
+# moves the joke from jokelist to postedjokes
+def vitsin_siirto(liian_pitk = False):
+        joke = kuvanteko.hae_vitsi()
+
+        # makes a csv for posted jokes if it doesnt exist
+        if os.path.exists("postedjokes.csv"):
+                print("postedjokes.csv exists")
+                tempdf = pd.read_csv("postedjokes.csv")
+                if liian_pitk == False:
+                        tempdf.loc[len(tempdf)] = joke
+                else:
+                        tempdf.loc[len(tempdf)] = "NOT POSTED" + joke
+                tempdf.to_csv("postedjokes.csv", index=False)
+        else:
+                tempdf = pd.DataFrame([joke])
+                tempdf.to_csv("postedjokes.csv",index=False)
+        # Drops the joke from the main joke list
+        aaaa = pd.read_csv("jokelist.csv")
+        aaaa = aaaa.loc[1:]
+        aaaa = aaaa.reset_index(drop = True)
+        aaaa.to_csv("jokelist.csv",index=False)
+
+joke = kuvanteko.hae_vitsi()
+
+# check if the font will be too small
+
+def tsekkaa():
+        if kuvanteko.etsi_font(kuvanteko.hae_vitsi()) < 24:
+                vitsin_siirto(liian_pitk = True)
+                return True
+        else:
+                return False
+
+while tsekkaa():
+        print("deleted'd joke because it was too long")
+
+
+
+## 50% chance konetta startatessa että tää postaa
+numba = np.random.normal(0)
+
+if numba > 0:
+        print("POSTAA!!!")               
+        path = kuvanteko.gen_kuva()
+
+
+
+        ## KUVAN POSTAUS
+
+
+        bot = instabot.Bot(filter_private_users=False)
+        bot.login(username = dadjokelogin[0], password = dadjokelogin[1])
+        time.sleep(5.5)
+        bot.upload_photo(path,caption='\r\n' +'\r\n' +'\r\n' + '#dad #joke #jokes #dadjokes #ai #machine #learning #artificial #intelligence #neural #network #funny #future #robot #humour')
+        ## moves the joke 
+        vitsin_siirto()
+        
+
+else:
+        print("ei postaa")
+
+
+
+
+
